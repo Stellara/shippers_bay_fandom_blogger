94e6458026081352d1a7977217e1975312a6301e
Erin Smith <erinsmith.8585@gmail.com>
2015-04-21 15:54:23 -0400

    DRY up article look up logic and cache value

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,11 @@
 class ArticlesController < ApplicationController
-  include ArticlesHelper
 
   def index 
     @articles = Article.all   
   end 
 
   def show 
-    retrieve_article
+    article
   end 
 
   def new 
@@ -14,21 +13,34 @@ class ArticlesController < ApplicationController
   end 
 
   def create 
-    @article = Article.create!(article_params)
+    @article = Article.create(article_params)
     redirect_to article_path(@article)
   end 
 
   def destroy 
-    retrieve_article.destroy
+    article.destroy
     redirect_to articles_path
   end 
 
   def edit 
-    retrieve_article
+    article
   end 
 
   def update 
-    retrieve_article.update(article_params)
-    redirect_to article_path(@article)
+    article.update(article_params)
+
+    flash.notice = "Article '#{article.title}' is updated!"
+
+    redirect_to article_path(article)
+  end 
+
+  private 
+
+  def article_params 
+    params.require(:article).permit(:title, :body)
+  end 
+
+  def article 
+    @article ||= Article.find(params[:id])
   end 
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,10 +1,2 @@
 module ArticlesHelper
-  def article_params 
-    params.require(:article).permit(:title, :body)
-  end 
-
-  def retrieve_article 
-    @article = Article.find(params[:id])
-    @article 
-  end 
 end
