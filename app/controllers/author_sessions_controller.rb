2bb76336e958d98d553387e20ea66a3fc14259ac
Erin Smith <erinsmith.8585@gmail.com>
2015-04-22 16:48:21 -0400

    Complete implementation of user creation and authentication

--- app/controllers/author_sessions_controller.rb
+++ app/controllers/author_sessions_controller.rb
@@ -4,8 +4,15 @@ class AuthorSessionsController < ApplicationController
 
   def create 
     if login(params[:email], params[:password])
+      redirect_back_or_to(articles_path, notice: "You are now logged in.")
+    else 
+      flash.now.alert = "Login failed. Please try again."
+      render action: :new
+    end 
   end 
 
   def destroy 
+    logout 
+    redirect_to(root, notice: "You are now logged in.")
   end 
 end
