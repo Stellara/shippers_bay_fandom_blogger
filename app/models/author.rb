6ecc5c05999befc4e6e1f88c0bf265659fe632a6
Erin Smith <erinsmith.8585@gmail.com>
2015-04-22 20:13:19 -0400

    Add email, username, and password validations to User

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,8 @@
 class Author < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  validates_confirmation_of :password, message: "Passwords should match", if: :password
+  validates_confirmation_of :password, presence: true, message: "Passwords should match", if: :password
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 6}
 end
